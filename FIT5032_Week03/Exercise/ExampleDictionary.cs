5548d37af8fa25981c7b6c492980de0d74bb4703
Ziqiao Yan <zyan0003@student.monash.edu>
2019-08-22 04:48:30 +1000

    TEST

---
+++ FIT5032_Week03/Exercise/ExampleDictionary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_Week03.Exercise
+{
+    public class ExampleDictionary
+    {
+        public void Example()
+        {
+            Dictionary<int, Student> studentDictionary = new Dictionary<int,
+            Student>();
+            Student s1 = new Student("Alex", "0431170991");
+            Student s2 = new Student("Kim", "0431170992");
+            studentDictionary.Add(1, s1);
+            studentDictionary.Add(2, s2);
+            Student result = new Student("", "");
+            studentDictionary.TryGetValue(1, out result);
+        }
+
+
+    }
+}
---
+++ FIT5032_Week03/Exercise/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_Week03.Exercise
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public Student(String name, String phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
